af4b59c6c82a2dbbe24511bd9318f58224c81014
Tanay Sapre <43600208+tansapre@users.noreply.github.com>
2023-10-31 10:13:23 -0700

    Update Code_smells_example.py

--- Code_smells_example.py
+++ Code_smells_example.py
@@ -3,8 +3,7 @@ def calculate_total_price(items):
     total = 0
     for item in items:
         if item.quantity > 0:
-            if item.price > 100:
-                total += item.price * 0.9
-            else:
-                total += item.price * 0.95
+            discount_rate = 0.9 if item.price > 100 else 0.95
+            discounted_price = item.price * discount_rate
+            total += discounted_price
     return total
